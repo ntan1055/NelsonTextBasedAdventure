8da77e509a718ebcbc19b7c7b7d4f6b0318019a1
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2018-12-03 08:43:23 -0500

    Update Main.java

--- src/Runner/Main.java
+++ src/Runner/Main.java
@@ -1,8 +1,21 @@
 package Runner;
 
-public class Main {
+import Player.Student;
+import Room.Classroom;
+import Room.Closet;
+import Room.Hallway;
+import Room.Lab;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main 
+{
+    private static boolean gameOn = true;
+    
+    public static void main(String[] args)
+    {
+        Classroom[][] Room = new Classroom [6][6];
+        Closet[][] Room1 = new Closet [4][4];
+        Hallway[][] Hallway = new Hallway [8][3];
+        Lab[][] Lab = new Lab [8][8];
+        
     }
 }
