65993f6996ed3c223e123e463aa1eaed7a71c0d9
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2018-12-04 08:27:24 -0500

    Update Main.java

--- src/Runner/Main.java
+++ src/Runner/Main.java
@@ -6,6 +6,8 @@ import Room.Closet;
 import Room.Hallway;
 import Room.Lab;
 
+import java.util.Scanner;
+
 public class Main 
 {
     private static boolean gameOn = true;
@@ -16,6 +18,81 @@ public class Main
         Closet[][] Room1 = new Closet [4][4];
         Hallway[][] Hallway = new Hallway [8][3];
         Lab[][] Lab = new Lab [8][8];
-        
+
+        Student Survivalist = new Student(" "," ", 0, 0);
+        Hallway[0][0].enterHallway(Student Survivalist);
+        Scanner in = new Scanner(System.in);
+        while(gameOn)
+        {
+            System.out.println("Where would you like to move? (Choose N, S, E, W");
+            String move = in.nextLine();
+            if(validMove(move, Survivalist, Hallway))
+            {
+                System.out.println("Your coordinates: row = " + Survivalist.getxLoc() + " col = " + Survivalist.getyLoc());
+            }
+            else
+            {
+                System.out.println("Please choose a valid move.");
+            }
+        }
+    }
+
+    public static boolean validMove(String move, Student x, Hallway[][] map)
+    {
+        move = move.toLowerCase().trim();
+        switch (move)
+        {
+            case "n":
+            if (x.getxLoc() > 0)
+            {
+                map[x.getxLoc()][x.getyLoc()].leaveSchool(x);
+                map[x.getxLoc()-1][x.getyLoc()].enterHallway(x);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            case "e":
+                if (x.getyLoc() < map[x.getyLoc()].length - 1)
+                {
+                    map[x.getxLoc()][x.getyLoc()].leaveSchool(x);
+                    map[x.getxLoc()][x.getyLoc() + 1].enterHallway(x);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            case "s":
+                if (x.getxLoc() < map.length - 1)
+                {
+                    map[x.getxLoc()][x.getyLoc()].leaveSchool(x);
+                    map[x.getxLoc() + 1][x.getyLoc()].enterHallway(x);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            case "w":
+                if (x.getyLoc() > 0)
+                {
+                    map[x.getxLoc()][x.getyLoc()].leaveSchool(x);
+                    map[x.getxLoc()][x.getyLoc() - 1].enterHallway(x);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            default:
+                break;
+        }
+        return true;
+    }
+    public static void gameOff()
+    {
+        gameOn = false;
     }
 }
